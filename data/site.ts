61bb26474093ec9f5a7bcf7ba37d9dda80c84fc3
Fazilfazi991 <fazilfazi991@gmail.com>
2026-08-09 09:42:58 +0400

    Replace duplicate logistics service imagery

--- data/site.ts
+++ data/site.ts
@@ -76,7 +76,7 @@ export const consultationPoints = [
 ];
 
 const visualPool = [
-  "/images/smart-logistics/freight-forwarding-port.png", "/images/smart-logistics/land-transportation-saudi.png", "/images/smart-logistics/customs-clearance.png", "/images/logistics/04_quote_team_variant_2.png", "/images/logistics/12_suspended_container.png", "/images/logistics/17_testimonial_worker_variant_2.png", "/images/logistics/07_testimonial_worker_variant_1.png", "/images/logistics/05_stacked_containers_variant_1.png"
+  "/images/smart-logistics/freight-forwarding-port.png", "/images/smart-logistics/land-transportation-saudi.png", "/images/smart-logistics/customs-clearance.png", "/images/smart-logistics/warehousing-3pl.png", "/images/smart-logistics/packing-moving.png", "/images/smart-logistics/equipment-rental.png", "/images/smart-logistics/equipment-maintenance.png", "/images/logistics/05_stacked_containers_variant_1.png"
 ];
 
 export const serviceDetails = services.map((service, index) => ({ ...service, image: visualPool[index], copy: [
